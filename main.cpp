5192c3e69a98920491bed2539acbed86873abec6
RyukNet <38398145+RyukNet@users.noreply.github.com>
2023-11-08 11:13:09 +0100

    Fixed a typo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char** argv){
-    std::cout << "Merzouk took lime one time and fell off of it twice ! ... and that's why kids you should never drive while drunk." << std::endl;
+    std::cout << "Merzouk took lime one time and fell off of it twice ! ... and that's why kids, you should never drive while drunk." << std::endl;
 
     return 0;
 }
