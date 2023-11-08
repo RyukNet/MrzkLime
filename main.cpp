0cc059ca5a58c0b19c02e7aeb50ab0781a08ab08
RaCharef <fa_charef@esi.dz>
2023-11-08 11:35:58 +0100

    remote empty line

--- main.cpp
+++ main.cpp
@@ -2,6 +2,5 @@
 
 int main(int argc, char** argv){
     std::cout << "Merzouk took lime one time and fell off of it twice ! ... and that's why kids, you should never drive while drunk." << std::endl;
-
     return 0;
 }
