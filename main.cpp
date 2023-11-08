c109c088ca6ddab40be026de4727f891170fe4c5
RaCharef <fa_charef@esi.dz>
2023-11-08 11:42:34 +0100

    replicate

--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 
 int main(int argc, char** argv){
     std::cout << "Merzouk took lime one time and fell off of it twice ! ... and that's why kids, you should never drive while drunk." << std::endl;
+     std::cout << "Merzouk took lime one time and fell off of it twice ! ... and that's why kids, you should never drive while drunk." << std::endl;
     return 0;
 }
