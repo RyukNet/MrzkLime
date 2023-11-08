efc8f5dd17e9ad3552713d0d4b9b449d0473a147
RyukNet <38398145+RyukNet@users.noreply.github.com>
2023-11-08 11:38:18 +0100

    Included ALGO lib

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <algorithm>
+
 int main(int argc, char** argv){
     std::cout << "Merzouk took lime one time and fell off of it twice ! ... and that's why kids, you should never drive while drunk." << std::endl;
 
