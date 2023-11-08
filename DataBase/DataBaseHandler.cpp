3be1fcdb499de8dba6595b8a8345788dd389d5c7
RyukNet <38398145+RyukNet@users.noreply.github.com>
2023-11-08 13:21:32 +0100

    First work on DataBaseHandler

--- DataBase/DataBaseHandler.cpp
+++ DataBase/DataBaseHandler.cpp
@@ -1,1 +1,14 @@
-#include "DataBaseHandler.h"s
+#include "DataBaseHandler.h"
+
+DataBaseHandler* DataBaseHandler::_instance = nullptr;
+
+void DataBaseHandler::initialize(){
+
+}
+
+DataBaseHandler* DataBaseHandler::instance(){
+    if(_instance == nullptr){
+        _instance = new DataBaseHandler();
+    }
+    return _instance;
+}
--- DataBase/DataBaseHandler.h
+++ DataBase/DataBaseHandler.h
@@ -1,1 +1,11 @@
 #include "common.h"
+
+class DataBaseHandler{
+public:
+    static void initialize();
+    static DataBaseHandler* instance();
+
+protected:
+    DataBaseHandler(){};
+    static DataBaseHandler* _instance;
+};
